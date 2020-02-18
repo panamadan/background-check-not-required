319493829b944752f597d6946900f0badf572308
Avery Stahl <averystahl@hotmail.com>
2020-02-18 14:07:40 -0500

    links going to routes

--- no_check/src/App.js
+++ no_check/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/Home/Home";
+import Signup from "./pages/Signup/Signup"
+import Signin from './pages/Signin/Signin';
 
 function App() {
   return (
@@ -10,7 +12,8 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/create" component={Home} />
-          <Route exact path="/signin" component={Home} />
+          <Route exact path="/signin" component={Signin} />
+          <Route exact path="/signup" component={Signup} />
         </Switch>
       </div>
     </Router>
