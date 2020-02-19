4f1c6834f91eedc8f04c3724743b5620a923264e
Juan De La Guardia <delaguardia@outlook.com>
2020-02-19 12:45:24 -0500

    first on wed

--- no_check/src/pages/CompanyInfo/Companyinfo.js
+++ no_check/src/pages/CompanyInfo/Companyinfo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import Form from 'muicss/lib/react/form';
 import Input from 'muicss/lib/react/input';
 import Textarea from 'muicss/lib/react/textarea';
@@ -7,19 +7,33 @@ import Button from 'muicss/lib/react/button';
 import Checkbox from 'muicss/lib/react/checkbox';
 import Container from 'muicss/lib/react/container';
 import Navbar from '../../components/Navbar/Navbar';
+// import axios from "axios";
 
 class CompanyInfo extends React.Component {
+
+  // handleSubmit(event){
+  //   event.preventDefault();
+  //   API.getJobs(jobsSearch)
+  //   .then(res => setJobs(res.data))
+  //   .catch(err => console.log(err));
+  // }
+
+  
   render() {
     return (
       <div>
         <Navbar />
         <Container>  
-          <Form>
+          <Form >
+            
             <legend>Company Info</legend>
             <Input placeholder="Name " />
             <Input placeholder="Job " />
             <Textarea placeholder="Description" />
-            <Button variant="raised">Submit</Button>
+            <Button 
+            // onSubmit= {handleSubmit}
+            
+            variant="raised">Submit</Button>
             <Checkbox name="inputA1" label="Felon" />
             <Checkbox name="inputA2" label="Misdemeanor" />
           </Form>
--- no_check/src/pages/Signup/Signup.js
+++ no_check/src/pages/Signup/Signup.js
@@ -1,10 +1,25 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
-
+// import ReactDOM from 'react-dom';
+import Form from 'muicss/lib/react/form';
+import Input from 'muicss/lib/react/input';
+import Textarea from 'muicss/lib/react/textarea';
+import Button from 'muicss/lib/react/button';
+import { Container } from "muicss/react";
 function Signup (){
     return(
         <div>
             <Navbar/>
+            <Container>
+            <Form>
+        <legend>Title</legend>
+        <Input label="Required Text Field" required={true} />
+        <Input label="Required Email Address" type="email" floatingLabel={true} required={true} />
+        <Textarea label="Required Textarea" floatingLabel={true} required={true} />
+        <Input label="Email Address" type="email" defaultValue="Validation error" />
+        <Button variant="raised">Submit</Button>
+      </Form>
+      </Container>
         </div>
     )
 }
