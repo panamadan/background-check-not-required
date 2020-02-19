81daa9d4ab157f4fcc0a783fae8e82e055c77fc7
Avery Stahl <averystahl@hotmail.com>
2020-02-19 12:19:35 -0500

    Added search / Created schema

---
+++ no_check/src/components/Search/Search.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../Search/Search.css";
+import Input from "muicss/lib/react/input";
+import Button from "muicss/lib/react/button";
+import Form from "muicss/lib/react/form"
+
+function Search()
+{
+
+
+    return(
+        <div>
+             <Form>
+                <Input className="searchVal" placeholder="Search for Jobs" />
+                <Button className="searchBtn" variant="raised">Search</Button>
+             </Form>
+        </div>
+    );
+};
+
+export default Search;
