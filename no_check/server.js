04146a7f483085f1f42c7958ad4ee157d8d78bd2
Avery Stahl <averystahl@hotmail.com>
2020-02-20 10:02:36 -0500

    created mongodb connection / deleted mysql things

--- no_check/server.js
+++ no_check/server.js
@@ -1,22 +1,33 @@
+// Dependencies
 const express = require("express");
-
 const mongoose = require("mongoose");
 const routes = require("./routes");
+
+// Require .env for mongoose
+require("dotenv").config;
+
+// Initialize express app
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Define middleware here
+// Define middleware encoding here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// Create connection to MongoDB
+const uri = process.env.ATLAS_URI
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
+
+const connection = mongoose.connection;
+connection.once("open", () => {console.log("MongoDB connection established successfully")});
+
 // Add routes, both API and view
 app.use(routes);
-
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 
 // Start the API server
 app.listen(PORT, function() {
