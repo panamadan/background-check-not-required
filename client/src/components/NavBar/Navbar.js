78112b8ab39d2a4d26445f6c4ac447ee131cb61f
Avery Stahl <averystahl@hotmail.com>
2020-02-25 14:36:13 -0500

    navbar updated

--- client/src/components/NavBar/Navbar.js
+++ client/src/components/NavBar/Navbar.js
@@ -1,30 +1,57 @@
 import React from "react";
-import Appbar from "muicss/lib/react/appbar";
 import "./Navbar.css";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
 
 function Navbar() {
-  let s1 = { verticalAlign: "middle", textAlign: "left", fontSize: 36 };
-  let s2 = { verticalAlign: "middle", textAlign: "right", fontSize: 30 };
+  const useStyles = makeStyles(theme => ({
+    '@global': {
+      ul: {
+        margin: 0,
+        padding: 0,
+        listStyle: 'none',
+      },
+    },
+    appBar: {
+      borderBottom: `1px solid ${theme.palette.divider}`,
+    },
+    toolbar: {
+      flexWrap: 'wrap',
+    },
+    toolbarTitle: {
+      flexGrow: 1,
+    },
+    link: {
+      margin: theme.spacing(1, 1.5)
+    }
+  }))
+
+  const classes = useStyles();
+
   return (
     <div>
-      <Appbar>
-        <table width="100%">
-          <tbody>
-            <tr style={s1}>
-              <td className="mui--appbar-height brandName">
-                <a href="/">No Background Check Required</a>
-              </td>
-              <td className="mui--appbar-height" style={s2}>
-                <Link to="/register">Register</Link>
-              </td>
-              <td className="mui--appbar-height" style={s2}>
-                <Link to="/login">Login </Link>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </Appbar>
+      <AppBar position="static" color="default" elevation={0} className={classes.appBar}>
+        <Toolbar className={classes.toolbar}>
+          <Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>
+            <Link href="/" color="textPrimary" className={classes.link}>
+              Background Check Not Required
+            </Link>
+          </Typography>
+          <nav>
+            <Link variant="button" color="textPrimary" href="/register" className={classes.link}>
+              Register
+            </Link>
+          </nav>
+          <Button href="/login" color="primary" variant="outlined" className={classes.link}>
+            Login
+          </Button>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
